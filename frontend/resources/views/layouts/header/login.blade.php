3a6fcce3a35e79a48222929985fe132aaa90abdb
Ajada <gonsalezajada@gmail.com>
2022-05-18 14:19:10 -0300

    Atualizção nos favicons

--- frontend/resources/views/layouts/header/login.blade.php
+++ frontend/resources/views/layouts/header/login.blade.php
@@ -4,8 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>X Solution EAD - Login</title>
-    <link rel="shortcut icon" href="{{ asset('__favicon.ico') }}" type="image/x-icon">
+    <title>X Solution EAD</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon//favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('favicon/site.webmanifest')}}">
+    <link rel="mask-icon" href="{{asset('favicon/safari-pinned-tab.svg')}}" color="#e90000">
+    <meta name="apple-mobile-web-app-title" content="Xsolution EAD">
+    <meta name="application-name" content="Xsolution EAD">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 </head>
--- frontend/resources/views/layouts/header/main.blade.php
+++ frontend/resources/views/layouts/header/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>X Solution EAD - {{-- $title --}}</title>
+    <title>X Solution EAD</title>
     <script src="https://kit.fontawesome.com/bc4fa83e4c.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     <link rel="stylesheet" href="{{ asset('css/blocks.css') }}">
@@ -14,7 +14,16 @@
     <link rel="stylesheet" href="{{ asset('css/modules/questions.css') }}">
     <link rel="stylesheet" href="{{ asset('css/modules/assessment.css') }}">
     <link rel="stylesheet" href="{{ asset('css/graphic/style.css') }}">
-    <link rel="shortcut icon" href="{{ asset('__favicon.ico') }}" type="image/x-icon">
+    {{-- <link rel="shortcut icon" href="{{ asset('__favicon.ico') }}" type="image/x-icon"> --}}
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon//favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('favicon/site.webmanifest')}}">
+    <link rel="mask-icon" href="{{asset('favicon/safari-pinned-tab.svg')}}" color="#e90000">
+    <meta name="apple-mobile-web-app-title" content="Xsolution EAD">
+    <meta name="application-name" content="Xsolution EAD">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body>
     @include('_partials.menu')
